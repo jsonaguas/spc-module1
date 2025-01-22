9b88e970d86d80f7ce6182da5312d5576446ac1b
jsonaguas <jaguas43@gmail.com>
2025-01-22 14:16:44 -0600

    added inventory table and association table

--- app/models.py
+++ app/models.py
@@ -9,6 +9,12 @@ class Base (DeclarativeBase):
 
 db = SQLAlchemy(model_class=Base)
 
+ticket_inventory = db.Table(
+    "ticket_inventory",
+    Base.metadata,
+    db.Column("service_ticket_id", db.ForeignKey("service_tickets.id")),
+    db.Column("inventory_id", db.ForeignKey("inventory.id"))
+)
 ticket_mechanic = db.Table(
     "ticket_mechanic",
     Base.metadata,
@@ -38,6 +44,7 @@ class ServiceTickets (Base):
 
     customer: Mapped["Customer"] = (db.relationship(back_populates='service_tickets'))
     mechanics: Mapped[List["Mechanic"]] = db.relationship(secondary=ticket_mechanic, back_populates='service_tickets')
+    inventory: Mapped[List["Inventory"]] = db.relationship('Inventory', secondary='ticket_inventory', back_populates='service_tickets')
 
 
 class Mechanic (Base):
@@ -50,5 +57,14 @@ class Mechanic (Base):
     salary: Mapped[float] = mapped_column(db.Float, nullable=False)
 
     service_tickets: Mapped[List["ServiceTickets"]] = db.relationship(secondary=ticket_mechanic, back_populates='mechanics')
+
+class Inventory (Base):
+    __tablename__ = 'inventory'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(db.String(100), nullable=False)
+    price: Mapped[float] = mapped_column(db.Float, nullable=False)
+
+    service_tickets: Mapped[List["ServiceTickets"]] = db.relationship('ServiceTickets', secondary='ticket_inventory', back_populates='inventory')
 
 
